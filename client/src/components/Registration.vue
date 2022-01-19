96733de1e13a6943d04e73d3221696f4a97aa6ec
Joel-Jensen <info@joeljensen.se>
2022-01-19 16:57:56 +0100

    Authentication

--- client/src/components/Registration.vue
+++ client/src/components/Registration.vue
@@ -1,10 +1,49 @@
 <template>
-  
+  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <img class="mx-auto h-36 w-auto" src="/src/assets/logo-black-text.svg" alt="Workflow" />
+        <h2 class="mt-12 text-center text-3xl font-semibold text-gray-900">
+          Create account
+        </h2>
+         <p class="mt-2 text-center text-sm text-gray-600">
+          You may only register with your ab1122-s@lu.se address
+        </p>
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true" />
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Email address" />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input id="password" name="password" type="password" autocomplete="new-password" required="" class="mt-3 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Password" />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Re-enter password</label>
+            <input id="password" name="password" type="password" autocomplete="new-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Re-type password" />
+          </div>
+        </div>
+
+        <div class="space-y-2">
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-teknikfokus-primary hover:bg-teknikfokus-primary-lightest focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teknikfokus-primary-light">
+            Create account
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
-
+  
 <script>
-export default {
+import { LockClosedIcon } from '@heroicons/vue/solid'
 
+export default {
+  components: {
+    LockClosedIcon,
+  },
 }
 </script>
 
--- client/src/components/dashboard/Login.vue
+++ client/src/components/dashboard/Login.vue
@@ -1,9 +1,65 @@
 <template>
-  
-</template>
+  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <img class="mx-auto h-36 w-auto" src="/src/assets/logo-black-text.svg" alt="Workflow" />
+        <h2 class="mt-12 text-center text-3xl font-semibold text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true" />
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Email address" />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Password" />
+          </div>
+        </div>
 
+        <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-teknikfokus-primary focus:ring-teknikfokus-primary-light border-gray-300 rounded" />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+              Remember me
+            </label>
+          </div>
+
+          <div class="text-sm">
+            <a href="#" class="font-medium text-teknikfokus-primary hover:text-teknikfokus-primary-light">
+              Forgot your password?
+            </a>
+          </div>
+        </div>
+
+        <div class="space-y-2">
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-teknikfokus-primary hover:bg-teknikfokus-primary-lightest focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teknikfokus-primary-light">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <LockClosedIcon class="h-5 w-5 text-gray-100" aria-hidden="true" />
+            </span>
+            Sign in
+          </button>
+          <div class="text-center w-full">
+            <router-link to="/registration" class="relative justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md hover:underline">
+              Create account
+            </router-link>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+  
 <script>
+import { LockClosedIcon } from '@heroicons/vue/solid'
+
 export default {
+  components: {
+    LockClosedIcon,
+  },
 }
 </script>
 
