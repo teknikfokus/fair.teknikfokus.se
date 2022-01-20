aaba27c53402950a711210d4c216df5a98838edb
FredrikHessner <Fredrik@hessner.se>
2022-01-20 16:02:30 +0100

    Duplicate email check - registration

--- src/auth/services/company_auth.service.ts
+++ src/auth/services/company_auth.service.ts
@@ -19,24 +19,43 @@ export class CompanyAuthService {
         private jwtService: JwtService,
     ) {}
 
-    registerCompanyAccount(user: CompanyUser): Observable<CompanyUser>{
-        const {email, password} = user;
-
-
-        return this.hashPassword(password).pipe(
-            switchMap((hashedPassword: string) => {
-                return from(this.companyRepository.save({
-                    email,
-                    password: hashedPassword,
-                })).pipe(
-                    map((user: CompanyUser) => {
-                        delete user.password;
-                        return user;
-                    }),
-                );
-            }),
-        );
+    doesUserExist(email: string): Observable<boolean> {
+      return from(this.companyRepository.findOne({ email })).pipe(
+        switchMap((user: CompanyUser) => {
+          return of(!!user);
+        }),
+      );
     }
+    registerCompanyAccount(user: CompanyUser): Observable<CompanyUser> {
+      const { email, password } = user;
+  
+      return this.doesUserExist(email).pipe(
+        tap((doesUserExist: boolean) => {
+          if (doesUserExist)
+            throw new HttpException(
+              'A user has already been created with this email address',
+              HttpStatus.BAD_REQUEST,
+            );
+        }),
+        switchMap(() => {
+          return this.hashPassword(password).pipe(
+            switchMap((hashedPassword: string) => {
+              return from(
+                this.companyRepository.save({
+                  email,
+                  password: hashedPassword,
+                }),
+              ).pipe(
+                map((user: CompanyUser) => {
+                  delete user.password;
+                  return user;
+                }),
+              );
+            }),
+          );
+        }),
+      );
+    }    
     validateUser(email: string, password: string): Observable<CompanyUser> {
         return from(
           this.companyRepository.findOne(
