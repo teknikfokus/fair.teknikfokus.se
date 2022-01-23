766f6e4e5a4bce49c20bfd334b3f7c62a6abab6c
Jakob Sinclair <sinclair.jakob@mailbox.org>
2022-01-23 17:23:53 +0100

    Allow company_id to be null
    Signed-off-by: Jakob Sinclair <sinclair.jakob@mailbox.org>

--- src/auth/models/company_user.entity.ts
+++ src/auth/models/company_user.entity.ts
@@ -5,14 +5,12 @@ export class CompanyUserEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({ nullable: true })
     company_id: number;
 
-    @Column({unique:true})
+    @Column({ unique: true })
     email: string;
 
     @Column({ select: false })
     password: string;
-
-
 }
