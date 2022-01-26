b72556d290db612019ab61ddb77ebcc150e80f47
Joel-Jensen <info@joeljensen.se>
2022-01-26 10:54:07 +0100

    Fix style

--- client/src/views/dashboard/company/Index.vue
+++ client/src/views/dashboard/company/Index.vue
@@ -2,7 +2,7 @@
   <section class="pt-8 px-4 max-w-5xl mx-auto">
     <nav-bar /> 
     <h1 class="mt-16 text-3xl font-semibold">Welcome to your dashboard</h1>
-    <div class="grid grid-cols-3 gap-4">
+    <div class="grid md:grid-cols-3 gap-4">
       <router-link to="/dashboard/company/Edit" class="text-center mt-6 relative inline-block justify-center py-6 px-12 border border-transparent text-lg font-medium rounded-md text-white bg-teknikfokus-primary hover:bg-teknikfokus-primary-lightest focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teknikfokus-primary-light">
         Edit profile
       </router-link>
--- client/src/views/dashboard/job/Create.vue
+++ client/src/views/dashboard/job/Create.vue
@@ -1,27 +1,40 @@
 <template>
-  <section class="pt-8 px-4 max-w-5xl mx-auto">
+  <section class="p-8 px-4 max-w-5xl mx-auto">
     <nav-bar />
-    <h1 class="mt-4 text-3xl font-semibold">Job description</h1><br/>
-    <editor
-       api-key="tjoqjkiq6re91rc0o4p5mxpe24smdqlqwv0c7f1w74pep3bo"
-       :init="{
-         height: 500,
-         menubar: false,
-         plugins: [
-           'advlist autolink lists link image charmap print preview anchor',
-           'searchreplace visualblocks code fullscreen',
-           'insertdatetime media table paste code help wordcount'
-         ],
-         toolbar:
-           'undo redo | formatselect | bold italic backcolor | \
-           alignleft aligncenter alignright alignjustify | \
-           bullist numlist outdent indent | removeformat | help'
-       }"
-        v-model="jobEditor"
-     />
-     <input v-model="jobTitle"  placeholder="Job title" class="mt-5"><br>
-    <router-link to="#" class="mt-3 relative inline-block justify-center py-2 px-7 border border-transparent text-lg font-medium rounded-md text-white bg-teknikfokus-primary hover:bg-teknikfokus-primary-lightest focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teknikfokus-primary-light">Send</router-link>
+    <div class="space-y-4">
 
+      <h1 class="mt-4 text-3xl font-semibold">Job description</h1>
+      <div>
+        <label for="title" class="block text-sm font-medium text-gray-700">Job Title</label>
+        <div class="mt-1">
+          <input v-model="jobTitle" type="text" name="title" id="title" class="focus:ring-teknikfokus-primary-light focus:border-tekring-teknikfokus-primary-light block w-full max-w-md px-2 sm:text-sm border-gray-300 rounded-md shadow-sm"/>
+        </div>
+      </div>
+
+      <div>
+        <label for="description" class="block text-sm font-medium text-gray-700 mb-1">Job Description</label>
+
+        <editor
+          id="description"
+          api-key="tjoqjkiq6re91rc0o4p5mxpe24smdqlqwv0c7f1w74pep3bo"
+          :init="{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar:
+              'undo redo | formatselect | bold italic backcolor | \
+              alignleft aligncenter alignright alignjustify | \
+              bullist numlist outdent indent | removeformat | help'
+          }"
+            v-model="jobEditor"
+        />
+      </div>
+      <router-link to="#" class="mt-3 relative inline-block justify-center py-2 px-7 border border-transparent text-lg font-medium rounded-md text-white bg-teknikfokus-primary hover:bg-teknikfokus-primary-lightest focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teknikfokus-primary-light">Send</router-link>
+    </div>
   </section>
 </template>
 
