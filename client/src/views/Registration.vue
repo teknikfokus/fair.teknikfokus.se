e2388a1b89248bda52eda59ab35c79ea5b373390
Joel-Jensen <info@joeljensen.se>
2022-01-26 10:04:39 +0100

    Login & register

--- client/src/views/Registration.vue
+++ client/src/views/Registration.vue
@@ -10,27 +10,33 @@
           You may only register with your ab1122-s@lu.se address
         </p>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
-        <input type="hidden" name="remember" value="true" />
+      <form class="mt-8 space-y-6" @submit.prevent="onSubmit" method="POST">
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Email address" />
+            <input id="email-address" v-model="form.email" name="email" type="email" autocomplete="email" required="" class="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Email address" />
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="new-password" required="" class="mt-3 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Password" />
+            <input id="password" v-model="form.password" name="password" type="password" autocomplete="new-password" required="" class="mt-3 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Password" />
           </div>
           <div>
-            <label for="password" class="sr-only">Re-enter password</label>
-            <input id="password" name="password" type="password" autocomplete="new-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Re-type password" />
+            <label for="re-password" class="sr-only">Re-enter password</label>
+            <input id="re-password" v-model="form.retypePassword" name="password" type="password" autocomplete="new-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Re-type password" />
           </div>
         </div>
 
         <div class="space-y-2">
+          <p v-if="error" class="text-red-600 text-sm text-center">{{ error }}</p>
+
           <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-teknikfokus-primary hover:bg-teknikfokus-primary-lightest focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teknikfokus-primary-light">
             Create account
           </button>
+          <div class="text-center w-full">
+            <router-link to="/login" class="relative justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md hover:underline">
+              Login
+            </router-link>
+          </div>
         </div>
       </form>
     </div>
@@ -39,11 +45,40 @@
   
 <script>
 import { LockClosedIcon } from '@heroicons/vue/solid'
+import http from '@/axios'
 
 export default {
   components: {
     LockClosedIcon,
   },
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+        retypePassword: '',
+      },
+      error: null,
+    }
+  },
+  methods: {
+    async onSubmit() {
+      if(this.form.password !== this.form.retypePassword) {
+        this.error = "Passwords doesn't match";
+        return;
+      }
+
+      try {
+        const res = await http.post("/registration", this.form);
+      } catch (err) {
+        if(err.response.status == 400) {
+          this.$router.push("/login");
+        } else if(err.response.status == 403) {
+          this.error = "The email needs to be an LU-email."
+        }
+      }
+    }
+  }
 }
 </script>
 
--- client/src/views/dashboard/Login.vue
+++ client/src/views/dashboard/Login.vue
@@ -7,46 +7,42 @@
           Sign in to your account
         </h2>
       </div>
-      <form class="mt-8 space-y-6" action="#" method="POST">
+      <form class="mt-8 space-y-6" @submit.prevent="onSubmit" method="POST">
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Email address" />
+            <input id="email-address" v-model="form.email" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Email address" />
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Password" />
+            <input id="password" v-model="form.password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-teknikfokus-primary-light focus:border-tekniring-teknikfokus-primary-light focus:z-10 sm:text-sm" placeholder="Password" />
           </div>
         </div>
 
         <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-teknikfokus-primary focus:ring-teknikfokus-primary-light border-gray-300 rounded" />
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900">
-              Remember me
-            </label>
-          </div>
-
           <div class="text-sm">
             <a href="#" class="font-medium text-teknikfokus-primary hover:text-teknikfokus-primary-light">
               Forgot your password?
             </a>
           </div>
+
+          <div>
+            <router-link to="/dashboard/registration" class="relative justify-center py-2 text-sm font-medium hover:underlin">
+              Create account
+            </router-link>
+          </div>
         </div>
 
         <div class="space-y-2">
+          <p v-if="error" class="text-red-600 text-sm text-center">{{ error }}</p>
+
           <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-teknikfokus-primary hover:bg-teknikfokus-primary-lightest focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teknikfokus-primary-light">
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <LockClosedIcon class="h-5 w-5 text-gray-100" aria-hidden="true" />
             </span>
             Sign in
           </button>
-          <div class="text-center w-full">
-            <router-link to="/registration" class="relative justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md hover:underline">
-              Create account
-            </router-link>
-          </div>
         </div>
       </form>
     </div>
@@ -55,11 +51,36 @@
   
 <script>
 import { LockClosedIcon } from '@heroicons/vue/solid'
+import http from '@/axios'
 
 export default {
   components: {
     LockClosedIcon,
   },
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+      error: null,
+    }
+  },
+  methods: {
+    async onSubmit() {
+      try {
+        const res = await http.post("/dashboard", this.form);
+        localStorage.setItem('token', JSON.stringify(res.data.token));
+        this.$router.replace("/dashboard/company");
+      } catch (err) {
+        if(err.response.status == 400) {
+          this.$router.push("/login");
+        } else if(err.response.status == 403) {
+          this.error = "Wrong credentials."
+        }
+      }
+    }
+  }
 }
 </script>
 
