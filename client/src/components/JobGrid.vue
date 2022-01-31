7d1b626e9ae913f812682cc87d31ffb98092d860
Joel-Jensen <info@joeljensen.se>
2022-01-31 09:50:01 +0100

    Fix padding

--- client/src/components/JobGrid.vue
+++ client/src/components/JobGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="jobs.length > 0" class="max-w-5xl mx-auto p-8">
+  <div v-if="jobs.length > 0" class="max-w-5xl mx-auto py-8">
     <h3 class="text-4xl mt-8 font-bold">Jobs</h3>
     <div class="mt-3 grid sm:grid-cols-2 md:grid-cols-3 gap-6">
       <router-link :to="'/companies/' + this.$route.params.id + '/job/' + job.id" v-for="job in jobs" :key="job.id" class="bg-white h-44 rounded-xl shadow-xl hover:shadow-2xl flex justify-center items-center text-xl font-medium text-center px-4">
