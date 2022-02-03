8396df1e905f51dc6b3f41c70397ba630610d5cd
Joel-Jensen <info@joeljensen.se>
2022-02-03 19:26:53 +0100

    Remove redirect on 403 errors

--- client/src/axios.js
+++ client/src/axios.js
@@ -23,7 +23,7 @@ http.interceptors.request.use((config) => {
 http.interceptors.response.use(function (response) {
   return response;
 }, function (error) {
-  if(error.response.status === 401 || error.response.status === 403) {
+  if(error.response.status === 401) {
     return localStorage.getItem('company_slug') || window.location.pathname.includes("/dashboard")
      ? router.push("/dashboard")
      : router.push("/");
