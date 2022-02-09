6cff4ab11e382fc9cc17c9deddde176a055d5060
Joel-Jensen <info@joeljensen.se>
2022-02-09 07:30:33 +0100

    Fix logo style

--- client/src/components/CompanyGrid.vue
+++ client/src/components/CompanyGrid.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="max-w-5xl mx-auto">
     <div v-if="link" class="mt-3 grid sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <router-link :to="'/companies/' + company.slug_name" v-for="company in companies" :key="company.slug_name" class="bg-white h-44 rounded-xl shadow-xl hover:shadow-2xl">
-        <img :src="endpoint + '/image/' + company.image_path" class="block mx-auto max-h-full max-w-full p-4"/>
+      <router-link :to="'/companies/' + company.slug_name" v-for="company in companies" :key="company.slug_name" class="flex justify-center items-center bg-white h-44 rounded-xl shadow-xl hover:shadow-2xl">
+        <img :src="endpoint + '/image/' + company.image_path" class="block mx-auto max-h-full max-w-full p-10"/>
       </router-link>
     </div>
     <div v-else class="mt-3 grid sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <div v-for="company in companies" :key="company.slug_name" class="bg-white h-44 rounded-xl shadow-xl hover:shadow-2xl">
-        <img :src="endpoint + '/image/' + company.image_path" class="block mx-auto max-h-full max-w-full p-4"/>
+      <div v-for="company in companies" :key="company.slug_name" class="flex justify-center items-center bg-white h-44 rounded-xl shadow-xl hover:shadow-2xl">
+        <img :src="endpoint + '/image/' + company.image_path" class="block mx-auto max-h-full max-w-full p-10"/>
       </div>
     </div>
   </div>
